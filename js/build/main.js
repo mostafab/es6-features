27a6fee84300b8827dbb77b65e332fcf4ea03ca4
mostafab <mostafa0104@gmail.com>
2016-06-02 19:54:54 -0400

    last commit

--- js/build/main.js
+++ js/build/main.js
@@ -1,45 +1,18 @@
-// Your code here!
 'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 (function () {
 
-    Array.prototype.map = Array.prototype.map || function (func) {
-        var newArr = [];
-        for (var i = 0; i < this.length; i++) {
-            newArr.push(func(this[i]));
-        }
-        return newArr;
-    };
-
-    Array.prototype.filter = Array.prototype.filter || function (func) {
-        var newArr = [];
-        for (var i = 0; i < this.length; i++) {
-            if (func(this[i])) {
-                newArr.push(func(this[i]));
-            }
-        }
-        return newArr;
-    };
-
-    Array.prototype.reduce = Array.prototype.reduce || function (func) {
-        var initialValue = arguments.length <= 1 || arguments[1] === undefined ? 0 : arguments[1];
-
-        var lastValue = initialValue;
-        for (var i = 0; i < this.length; i++) {
-            lastValue = func(lastValue, this[i]);
-        }
-        return lastValue;
-    };
-
     var patients = [{ name: 'Inspector Javert', dob: 'Jan' }, { name: 'Billy Pilgrim', dob: 'Feb' }, { name: 'Sancho Panza', dob: 'Jan' }];
 
-    var dog = { name: 'Spot', breed: 'Pomeranian' };
-
-    var name = dog.name;
-    var breed = dog.breed;
-
-
-    console.log('Hello, ' + name + ', you\'re a ' + breed);
+    var string = '';
+    patients.forEach(function (patient) {
+        return string += 'This is ' + patient.name + '. He was born in ' + patient.dob + '\n';
+    });
+    console.log(string);
 
     var sum = function sum() {
         for (var _len = arguments.length, nums = Array(_len), _key = 0; _key < _len; _key++) {
@@ -50,16 +23,43 @@
             return value + current;
         });
     };
-
     console.log(sum(1, 2, 3, 4, 5, 6, 7, 8));
 
-    var filteredPatients = patients.filter(function (patient) {
+    var filteredPatientNames = patients.filter(function (patient) {
         return patient.dob === 'Jan';
     }).map(function (patient) {
         return patient.name;
     });
 
-    console.log(filteredPatients.length);
-})();
+    console.log(filteredPatientNames.length);
 
-// console.log(Array.prototype.map);
+    var PetOwner = function () {
+        function PetOwner(name) {
+            _classCallCheck(this, PetOwner);
+
+            this.name = name;
+            this.pets = [{ petName: 'Muffin', type: 'Dog' }, { petName: 'Garfield', type: 'Cat' }] || [];
+        }
+
+        _createClass(PetOwner, [{
+            key: 'meetPets',
+            value: function meetPets() {
+                var _this = this;
+
+                this.pets.forEach(function (pet) {
+                    return console.log(_this);
+                });
+            }
+        }]);
+
+        return PetOwner;
+    }();
+
+    var George = new PetOwner('George');
+    George.meetPets();
+
+    for (var _i = 0; _i < 4; _i++) {
+        console.log(_i);
+    }
+    console.log(i);
+})();
--- js/es6/main.js
+++ js/es6/main.js
@@ -1,34 +1,4 @@
-﻿// Your code here!
-'use strict';
-
-
-(function() {
-    
-    Array.prototype.map = Array.prototype.map || function(func) {
-        let newArr = [];
-        for (let i = 0; i < this.length; i++) {
-            newArr.push(func(this[i]));
-        }
-        return newArr;
-    }
-
-    Array.prototype.filter = Array.prototype.filter || function(func) {
-        let newArr = [];
-        for (let i = 0; i < this.length; i++) {
-            if (func(this[i])) {
-                newArr.push(func(this[i]));   
-            }
-        }
-        return newArr;
-    }
-
-    Array.prototype.reduce = Array.prototype.reduce || function(func, initialValue = 0) {
-        let lastValue = initialValue;
-        for (let i = 0; i < this.length; i++) {
-            lastValue = func(lastValue, this[i]);
-        }
-        return lastValue;
-    }
+﻿(() => {
 
     let patients = [
         {name: 'Inspector Javert', dob: 'Jan'},
@@ -36,20 +6,40 @@
         {name: 'Sancho Panza', dob: 'Jan'}
     ]
 
-    const dog = { name: 'Spot', breed: 'Pomeranian' };
-
-    const {name, breed} = dog;
-    
-    console.log(`Hello, ${name}, you're a ${breed}`);
+    let string = '';
+    patients.forEach(
+        patient => string += `This is ${patient.name}. He was born in ${patient.dob}\n`
+        );
+    console.log(string);
 
     let sum = (...nums) => nums.reduce((value, current) => value + current);
-
     console.log(sum(1, 2, 3, 4, 5, 6, 7, 8));
     
-    let filteredPatients = patients.filter(patient => patient.dob === 'Jan').map(patient => patient.name);
+    let filteredPatientNames = patients
+        .filter(patient => patient.dob === 'Jan').map(patient => patient.name);
     
-    console.log(filteredPatients.length);
+    console.log(filteredPatientNames.length);
+
+    class PetOwner {
+        constructor(name) {
+            this.name = name;
+            this.pets = [
+                {petName: 'Muffin', type: 'Dog'},
+                {petName: 'Garfield', type: 'Cat'}
+            ] || []
+        }
+
+        meetPets() {
+            this.pets.forEach(pet => console.log(this));
+        }
+    }
+    const George = new PetOwner('George');
+    George.meetPets();
+
+    for (let i = 0; i < 4; i++) {
+        console.log(i);
+    }
+    console.log(i);
     
 })();
 
-// console.log(Array.prototype.map);
